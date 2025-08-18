787cb0251c903bbc887503f6f7094d98a4e7825e
missionljm <1781137162@qq.com>
2025-08-18 14:04:44 +0800

    init project

---
+++ src/main/java/com/mu/musmart/MuSmartApplication.java
@@ -0,0 +1,13 @@
+package com.mu.musmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuSmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mu/musmart/MuSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mu.musmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
